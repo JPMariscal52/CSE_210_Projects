84387ce62a9e14eeb09d8f7dd78dd3feebecaf17
JPMariscal52 <clavijero8@gmail.com>
2024-02-04 00:36:10 -0600

    Learning activity Week 04 Inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assigment1 = new Assignment("Juan Carlos", "Ecuations");
+        string summary = assigment1.GetSummary();
+
+        MathAssignment mAssignment = new MathAssignment("4.5", "1-10","Tania Rodriguez", "Addition");
+        string mSummary = mAssignment.GetSummary();
+        string mHomeWorkList = mAssignment.GetHomeworkList();
+
+        WritingAssignment wAssignment = new WritingAssignment("Good behavior", "Jafet Muñiz", "Human Nature");
+        string wSummary = wAssignment.GetSummary();
+        string wInformation = wAssignment.GetWritingInformation();
+
+
+        Console.WriteLine(summary);
+        Console.WriteLine(mSummary);
+        Console.WriteLine(mHomeWorkList);
+        Console.WriteLine(wSummary);
+        Console.WriteLine(wInformation);
+
     }
 }
