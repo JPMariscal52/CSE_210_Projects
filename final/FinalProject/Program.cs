a7048f5e491fb65105be1804d06af577ce143027
JPMariscal52 <clavijero8@gmail.com>
2024-02-18 01:41:20 -0600

    Prove milestone week 6

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        
+        string option = "";
+
+        do
+        {
+            Console.WriteLine("Welcome to the Church Maintenance Manangement System");
+
+            Console.WriteLine("1. Facilities supervisor");
+            Console.WriteLine("2. Church leader");
+            Console.WriteLine("3. Facilities Technician");
+            Console.WriteLine("4. Quit");
+
+            Console.WriteLine("Please select your charge: ");
+            option = Console.ReadLine();
+
+        } while (option != "4");
+
+
+
     }
 }
