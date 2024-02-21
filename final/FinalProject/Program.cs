43559af67cc95df85c8331b352712eebe2148f09
JPMariscal52 <clavijero8@gmail.com>
2024-02-21 00:18:08 -0600

    Adding Readme Final Project

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -13,18 +13,6 @@ class Program
         {
             Console.Clear();
 
-            //In this section you can select the profile that you will use.
-            //If you are a Facilities Supervisor you can visualize all maintenance requests and assign a technician for each request,
-            // also you can save and load all data of the program.
-            //If you are a Church Leader you can ask one or many requests, and also you can record the request.
-            //If you are Facilities technician you can visualize all technicians and cases assigned, also you can record maintenance services.
-            //The first step you have to do is to enter one or many requests, the same church leader can enter one or many requests.
-            //The second step is to assign a facilities technician to a request in the supervisor section, you can assign the technician to different requests.
-            //The list request has two validation boxes, one for church leader and other for technician, the request only will be done if two boxes are checked,
-            //if just one is checked, the supervisor has to clarify the reason.
-            //The last step is the technician section, there you can visualize all technicians, and requests, also you can record them.
-
-
             Console.WriteLine("Welcome to the Church Maintenance Manangement System");
             Console.WriteLine();
             Console.WriteLine("1. Facilities Supervisor");
